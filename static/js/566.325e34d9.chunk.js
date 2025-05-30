20e3f453de04ff772163ffd8c59d2fa4ca8e8ecf
Josue Daniel Martinez <overlordprice014@gmail.com>
2025-05-30 01:27:48 -0600

    Updates

---
+++ static/js/566.325e34d9.chunk.js
@@ -0,0 +1,49 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[566],{6566:(e,t,i)=>{i.r(t),i.d(t,{default:()=>u});var n=i(4647),r=i(1319),s=i(3614),d=i(9281),c=i(5639),a=i(2646),l=i(9);const o=(0,l.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,h=(0,l.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,x=(0,l.Ay)(r.A)`
+  flex-direction: ${e=>{let{direction:t}=e;return"left"===t?"row":"row-reverse"}};
+`,p=(0,l.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,m=(0,l.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,y=(0,l.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,j=(0,l.Ay)("p")`
+  font-size: 13px;
+`,w=(0,l.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`,f=(0,l.Ay)("img")`
+  display: block;
+  max-width: 100%;
+  height: auto;
+`;var g=i(579);const u=(0,d.C)()((e=>{let{icon:t,title:i,content:d,section:l,button:u,t:A,id:b,direction:v}=e;return(0,g.jsx)(o,{children:(0,g.jsx)(s.zW,{direction:v,triggerOnce:!0,children:(0,g.jsxs)(x,{justify:"space-between",align:"middle",id:b,direction:v,children:[(0,g.jsx)(n.A,{lg:11,md:11,sm:12,xs:24,children:(e=>{try{return new URL(e),!0}catch(t){return!1}})(t)?(0,g.jsx)(f,{src:t,alt:A(i),width:"100%",height:"auto"}):(0,g.jsx)(a.A,{src:t,width:"100%",height:"100%"})}),(0,g.jsx)(n.A,{lg:11,md:11,sm:11,xs:24,children:(0,g.jsxs)(p,{children:[(0,g.jsx)("h6",{children:A(i)}),(0,g.jsx)(h,{children:A(d)}),"right"===v?(0,g.jsx)(w,{children:"object"===typeof u&&u.map(((e,t)=>(0,g.jsx)(c.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("about"),children:A(e.title)},t)))}):(0,g.jsx)(m,{children:(0,g.jsx)(r.A,{justify:"space-between",children:"object"===typeof l&&l.map(((e,t)=>(0,g.jsxs)(n.A,{span:11,children:[(0,g.jsx)(a.A,{src:e.icon,width:"60px",height:"60px"}),(0,g.jsx)(y,{children:A(e.title)}),(0,g.jsx)(j,{children:A(e.content)})]},t)))})})]})})]})})})}))}}]);
+//# sourceMappingURL=566.325e34d9.chunk.js.map
